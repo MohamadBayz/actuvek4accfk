66831941687e4563a19533c5ca951b0c48eca79d
MohamadBayz <63660736+MohamadBayz@users.noreply.github.com>
2020-05-29 09:25:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    tokens: ["NzE1ODA3OTE3MjY1MTI1Mzg4.XtCmxw.YZSR6dnh4pvQN1clqwnhxZFbl5I"], ///توكنات حسابات الترحيب
+    tokens: ["NzE1ODA3OTE3MjY1MTI1Mzg4.XtCqjQ.xJCW8ms9LRFK7urY6UMqHE12cIM"], ///توكنات حسابات الترحيب
     voice_channel: "707254827872157787", ///ايدي الروم اللي بدك يثبت فيه
     text_channel: "686463796872740874", ///ايدي روم اللي بدك يرحب فيه
     your_server: "707253754528923648", /// ايدي السيرفر تبعك
